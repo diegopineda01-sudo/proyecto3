f41e654c5a295e1db68386a8f7e29def59d069ee
diego <diego.pineda01@globant.com>
2025-11-13 14:31:05 +0100

    fiinalchange

--- src/main.js
+++ src/main.js
@@ -1,32 +1,37 @@
 import './style.css'
 
+import img1 from '/img/aiony-haust-3TLl_97HNJo-unsplash.jpg';
+import img2 from '/img/michael-dam-mEZ3PoFGs_k-unsplash.jpg';
+import img3 from '/img/ivana-cajina-_7LbC5J-jw4-unsplash.jpg';
+import img4 from '/img/ian-dooley-d1UPkiFd04A-unsplash.jpg';
+
 const reviews = [
   {
     id: 1,
     name: 'susan smith',
     job: 'web developer',
-    img: 'img/aiony-haust-3TLl_97HNJo-unsplash.jpg',
+    img: img1,
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: 'anna johnson',
     job: 'web designer',
-    img: 'img/michael-dam-mEZ3PoFGs_k-unsplash.jpg',
+    img: img2,
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
     id: 3,
     name: 'peter jones',
     job: 'intern',
-    img: 'img/ivana-cajina-_7LbC5J-jw4-unsplash.jpg',
+    img: img3,
     text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
     id: 4,
     name: 'bill anderson',
     job: 'the boss',
-    img: 'img/ian-dooley-d1UPkiFd04A-unsplash.jpg',
+    img: img4,
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
